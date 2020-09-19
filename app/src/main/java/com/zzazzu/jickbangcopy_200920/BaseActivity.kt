32d4e87cf36aa0c1840bdccd6390418c9fbf5030
bucephalus41 <bucephalus41@gmail.com>
2020-09-19 16:21:39 +0900

    초기 데이터 입력

--- app/src/main/java/com/zzazzu/jickbangcopy_200920/BaseActivity.kt
+++ app/src/main/java/com/zzazzu/jickbangcopy_200920/BaseActivity.kt
@@ -2,5 +2,12 @@ package com.zzazzu.jickbangcopy_200920
 
 import androidx.appcompat.app.AppCompatActivity
 
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
 }
